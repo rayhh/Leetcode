5e4d68ba0dcd8b2f31e416e25fcee2717d0f19b5
Ray_h <leihao22@126.com>
2020-02-18 23:39:25 +0800

    二叉树的后序遍历

---
+++ 145.cpp
@@ -0,0 +1,23 @@
+//用一个多余的节点存储数组中最后一个元素所对应的节点，即最后访问的一个节点
+
+vector<int> postorderTraversal(TreeNode* root){
+	vecotr<int> res;
+	TreeNode* pre=nullptr;
+	stack<TreeNode*> s;
+	while(root||!s.empty()){
+		if(root){
+			s.push(root);
+			root=root->left;
+		}else{
+			root=s.top();
+			if(root->right&&root->right!=pre) root=root->right;
+			else{
+				res.push_back(root->val);
+				pre=root;
+				s.pop();
+				root=nullptr;
+			}
+		}
+	}
+	return res;
+}
