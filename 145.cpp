fe6c25a80647c4ad911f73c04ee6d693374ecad5
Ray_h <leihao22@126.com>
2020-02-18 23:39:54 +0800

    二叉树的后序遍历

--- 145.cpp
+++ 145.cpp
@@ -1,7 +1,7 @@
 //用一个多余的节点存储数组中最后一个元素所对应的节点，即最后访问的一个节点
 
 vector<int> postorderTraversal(TreeNode* root){
-	vecotr<int> res;
+	vector<int> res;
 	TreeNode* pre=nullptr;
 	stack<TreeNode*> s;
 	while(root||!s.empty()){
