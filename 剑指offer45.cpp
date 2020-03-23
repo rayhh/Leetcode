6bdd23185c6b4d4adba9d07bf1511d067f1c9e14
Ray_h <leihao22@126.com>
2020-03-23 16:38:01 +0800

    把数组排成最小的数

---
+++ 剑指offer45.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string minNumber(vector<int>& nums) {
+        vector<string> strs;
+        for(auto x : nums)
+        strs.push_back(to_string(x));
+        sort(strs.begin(),strs.end(),cmp);
+        string res = "";
+        for(auto c : strs)
+        res += c;
+        return res;
+
+    }
+//此处需要设置为静态函数
+    static bool cmp(const string& a,const string& b){
+        return a + b < b + a;
+    }
+};
